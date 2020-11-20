657af78805781ec4b51604beaf97efb84943478e
Dustin Brown <dbrown@idauto.net>
2020-11-20 09:53:48 -0600

    Adding Menu

--- src/main/java/com/dustintbrown/app/App.java
+++ src/main/java/com/dustintbrown/app/App.java
@@ -2,64 +2,38 @@ package com.dustintbrown.app;
 
 import com.dustintbrown.app.util.OIDService;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.time.Duration;
+import java.util.Collection;
+import java.util.Scanner;
+import java.util.logging.LogManager;
 import java.util.logging.Logger;
 
 public class App {
-//    private final static Logger LOGGER = Logger.getLogger(App.class.getName());
-
     private final static Logger LOGGER = Logger.getLogger(App.class.getName());
-
+    private static OIDService oidService = new OIDService();
     public static void main(String[] args) {
-        OIDService oidService = new OIDService();
-        final long startTime = System.currentTimeMillis();
-
         //
-        // Print OIDS
+        // Setup Logger
         //
-
-//        System.out.println(Arrays.toString(oids.toArray()));
-
-        //
-        // Write OIDS to file
-        //
-
-//        try {
-//            oidService.populateWithRandomOIDs(10);
-//            oidService.writeOIDsToFile("/Users/dustintbrown/Desktop/10_oids.txt");
-//        } catch (Exception e) {
-//            LOGGER.severe(e.getMessage());
-//            e.printStackTrace();
-//        }
-
-        //
-        // Import OIDs from a file
-        //
-
         try {
-            oidService.importOIDsFromFile("/Users/dustintbrown/Desktop/1-million_oids.txt");
-        } catch (Exception e) {
+            ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+            InputStream input = classLoader.getResourceAsStream("logger.properties");
+            LogManager.getLogManager().readConfiguration(input);
+        }catch(IOException e){
             LOGGER.severe(e.getMessage());
             e.printStackTrace();
         }
 
         //
-        // Does 1.6.7.4.5.44626 exist in set?
+        // Show Menu
         //
-
-//        LOGGER.info("exists (6.5.4.9.5.14403.12885.19823.26314): " + oidService.exists("6.5.4.9.5.14403.12885.19823.26314"));
-
-        //
-        // Find Prefixes
-        //
-
-//        oidService.getOidsWithPrefix("9.9");
-//        oidService.getOidsWithPrefix("1.2.3");
-//        Collection<String> matches = oidService.getOidsWithPrefix("6.5.4.9");
+        showMenu();
 
 
-        final long endTime = System.currentTimeMillis();
-        Duration duration = Duration.ofMillis(endTime - startTime);
+
 
 //        LOGGER.info("Total OIDs Loaded: " + oidService.getOids().size());
 //        LOGGER.info("Matches Found: ");
@@ -67,7 +41,116 @@ public class App {
 //        LOGGER.info(matches.toString());
 //        oidService.populateWithRandomOIDs(1);
 //        oidService.getOids().forEach(t -> LOGGER.info(oidService.parseOID(t)));
-        oidService.encode();
-        LOGGER.info("Total execution time: " + duration.toHoursPart() + "h " + duration.toMinutesPart() + "m " + duration.toSecondsPart() + "s " + duration.toMillisPart() + "ms");
+//        oidService.encode();
+//        LOGGER.info("Total execution time: " + duration.toHoursPart() + "h " + duration.toMinutesPart() + "m " + duration.toSecondsPart() + "s " + duration.toMillisPart() + "ms");
+    }
+
+    private static void showMenu(){
+        System.out.println("Choose from these choices");
+        System.out.println("-------------------------");
+        System.out.println("1 - Load OIDs from file");
+        System.out.println("2 - Load random OIDs");
+        System.out.println("3 - Write loaded OIDs to a file");
+        System.out.println("4 - Print loaded OIDs to the console");
+        System.out.println("5 - Check if OID exists in loaded data");
+        System.out.println("6 - Display OIDs in data matching a prefix");
+        System.out.println("7- Show diff between loaded OIDs and a given file");
+        System.out.println("8 - Quit");
+
+        Scanner scanner = new Scanner(System.in);
+        int choice = scanner.nextInt();
+
+        switch (choice) {
+            case 1:
+                //
+                // Import OIDs from a file
+                //
+                if(oidService.getOids().size()>0){
+                    System.out.println("WARNING: This will clear existing loaded OIDs");
+                }
+                try {
+                    System.out.println("Enter a filename");
+                    Scanner scanner1= new Scanner(System.in);
+                    String filename = scanner1.nextLine();
+                    oidService.importOIDsFromFile(filename);
+                } catch (Exception e) {
+                    LOGGER.severe(e.getMessage());
+                    e.printStackTrace();
+                }
+                break;
+            case 2:
+                //
+                // Load from random generation
+                //
+                if(oidService.getOids().size()>0){
+                    System.out.println("WARNING: This will clear existing loaded OIDs");
+                }
+                System.out.println("How many OIDs would you like to generate?");
+                Scanner scanner2= new Scanner(System.in);
+                oidService.populateWithRandomOIDs(scanner2.nextInt());
+                break;
+            case 3:
+                //
+                // Write OIDS to file
+                //
+                Scanner scanner3= new Scanner(System.in);
+                String filename = scanner3.nextLine();
+                try {
+                    oidService.populateWithRandomOIDs(10);
+                    oidService.writeOIDsToFile(filename);
+                } catch (Exception e) {
+                    LOGGER.severe(e.getMessage());
+                    e.printStackTrace();
+                }
+                break;
+            case 4:
+                //
+                // Print OIDs
+                //
+                OIDService.printOIDs(oidService.getOids());
+                break;
+            case 5:
+                //
+                // Does OID exist
+                //
+                if(oidService.getOids().size()<1){
+                    System.out.println("You must first load OIDs.");
+                    break;
+                }
+                System.out.println("Enter OID to find");
+                Scanner scanner5= new Scanner(System.in);
+                String oid = scanner5.nextLine();
+                System.out.println("Does " + oid + " appear in the set: " + oidService.exists(oid));
+                break;
+            case 6:
+                //
+                // Return all OIDs with matching prefix
+                //
+                System.out.println("Enter prefix to find");
+                Scanner scanner6= new Scanner(System.in);
+                String prefix = scanner6.nextLine();
+                Collection<int[]> matches = oidService.getOidsWithPrefix(prefix);
+                if(matches.size()>0){
+                    System.out.println(matches.size() + " found. Would you like to output them to the console? (y/n)");
+                    Scanner scanner6a= new Scanner(System.in);
+                    if(scanner6a.nextLine().startsWith("y")){
+                        OIDService.printOIDs(matches);
+                    }
+                }else{
+                    System.out.println("No matches found.");
+                }
+                break;
+            case 7:
+                // Generate diff
+                break;
+            case 8:
+                // Perform "quit" case.
+                System.exit(0);
+            default:
+                // The user input an unexpected choice.
+                showMenu();
+        }
+        System.out.println("");
+        showMenu();
     }
 }
--- src/main/java/com/dustintbrown/app/util/OIDService.java
+++ src/main/java/com/dustintbrown/app/util/OIDService.java
@@ -24,28 +24,53 @@ public class OIDService {
 
     // replaces set with generated data
     public void populateWithRandomOIDs(int num) {
+        long startTime = System.currentTimeMillis();
         LOGGER.log(Level.INFO, "Starting generation of {0} oids.", num);
         oids.clear();
         while (oids.size() < num) {
             oids.add(generateSingleOID());
         }
-        LOGGER.log(Level.INFO, "Finished generation of oids.");
+        long endTme = System.currentTimeMillis();
+        LOGGER.log(Level.INFO, "Finished generation of oids in {0}ms.",(endTme-startTime));
     }
 
     private int[] generateSingleOID() {
-        // First 4 sections are 1-15
-        // 5-10 total sections
+        // First 2 sections are 1-15
+        // 3-10 total sections
         // each section after  is a random number from 1-Integer.MAX_VALUE
         int length = rand.nextInt(6) + 4;
         int[] returnMe = new int[length];
         for (int i = 0; i < length; i++) {
-            if (i < 5) {
+            if (i < 2) {
                 returnMe[i] = (rand.nextInt(15) + 1);
-            } else {
-                returnMe[i] = (rand.nextInt(Integer.MAX_VALUE) + 1);
+            } else if(i==length-1) {
+                //last item should have a larger scope to be more realistic
+                returnMe[i] = generateBiasedIntGTZero(65000,4096);
+            }else{
+                //middle numbers should be biased toward 0 give the way these numbers appear in the wild.
+                returnMe[i] = generateBiasedIntGTZero(255 * i,128);
             }
         }
         return returnMe;
+    }
+
+    private int generateBiasedIntGTZero(int max, int bias_threshold){
+        int cnt = 1;
+        int next = 0;
+        while(cnt<6){ //basically a poor man's weighting toward numbers lower than start_max
+            next = rand.nextInt(max) + 1;
+            if(next<(bias_threshold*cnt)){
+                break;
+            }
+            cnt++;
+        }
+        return next;
+    }
+
+    public static void printOIDs(Collection<int[]> oidsToPrint){
+        oidsToPrint.forEach(oid -> {
+            System.out.println(OIDService.parseOID(oid));
+        });
     }
 
     public void importOIDsFromFile(String filename) throws FileNotFoundException, IOException {
@@ -59,7 +84,7 @@ public class OIDService {
                 oids.add(parseOIDString(line));
                 record_count++;
                 if (record_count % 1000000 == 0) {
-                    LOGGER.log(Level.INFO, "Read {0} records.", record_count);
+                    LOGGER.log(Level.FINE, "Read {0} records.", record_count);
                 }
             }
         }
@@ -68,7 +93,14 @@ public class OIDService {
         LOGGER.log(Level.INFO, "Read {0} OIDs from file. Operation completed in {1} milliseconds.", params);
     }
 
-    public int[] parseOIDString(String oid) {
+    public void writeOIDsToFile(String filename) throws IOException {
+        FileWriter fileWriter = new FileWriter(filename);
+        try(PrintWriter printWriter = new PrintWriter(fileWriter)){
+            oids.forEach(oid -> printWriter.print(parseOID(oid)));
+        }
+    }
+
+    public static int[] parseOIDString(String oid) {
         String[] parts = oid.split("\\.");
         int[] returnMe = new int[parts.length];
         for (int i = 0; i < parts.length; i++) {
@@ -77,7 +109,7 @@ public class OIDService {
         return returnMe;
     }
 
-    public String parseOID(int[] oid) {
+    public static String parseOID(int[] oid) {
         String returnMe = "";
         for (int i = 0; i < oid.length; i++) {
             returnMe += oid[i];
@@ -98,14 +130,14 @@ public class OIDService {
     /*
         Solution for Question #3
      */
-    public Collection<String> getOidsWithPrefix(String prefix) {
+    public Collection<int[]> getOidsWithPrefix(String prefix) {
         int[] start = parseOIDString(prefix);
         int[] end = Arrays.copyOf(start, start.length + 1);
         end[end.length - 1] = Integer.MAX_VALUE;
         LOGGER.log(Level.INFO, "Searching for {0}", prefix);
         final long startTime = System.currentTimeMillis();
-        Set<int[]> subset = oids.subSet(start, end);
-        Set<String> returnMe = subset.stream().map(this::parseOID).collect(Collectors.toSet());
+        Set<int[]> returnMe = oids.subSet(start, end);
+//        Set<String> returnMe = subset.stream().map(OIDService::parseOID).collect(Collectors.toSet());
         final long endTime = System.currentTimeMillis();
         Object[] params = {returnMe.size(), (endTime - startTime)};
         LOGGER.log(Level.INFO, "Found {0} results in {1} milliseconds", params);
@@ -139,20 +171,9 @@ public class OIDService {
                 //single byte encoding
                 data.add((byte) oid[i]);
             } else if (oid[i] <= 16383) {
-                encodeComplexInt(data, oid[i]);
+                encodeTwoByteInt(data, oid[i]);
             } else {
-                int step1 = oid[i] / 262144;
-                int txMg = 0x8 + step1;
-                int base = 262144 * step1;
-                int step2 = oid[i] - base;
-                int txMi = step2 / 16384;
-
-                String hexVal = "0x" + Integer.toString(txMg) + Integer.toString(txMi);
-                data.add((byte) Integer.decode(hexVal).intValue());
-
-                base = base + (txMi * 16384);
-                int step3 = oid[i] - base;
-                encodeComplexInt(data, step3);
+                encodeFourByteInt(data, oid[i]);
             }
         }
         data.set(1, (byte) (data.size() - 2));
@@ -166,8 +187,22 @@ public class OIDService {
 
         return returnMe;
     }
+    private void encodeFourByteInt(ArrayList<Byte> data, int i) {
+        int step1 = i / 262144;
+        int txMg = 0x8 + step1;
+        int base = 262144 * step1;
+        int step2 = i - base;
+        int txMi = step2 / 16384;
 
-    private void encodeComplexInt(ArrayList<Byte> data, int i) {
+        String hexVal = "0x" + Integer.toString(txMg) + Integer.toString(txMi);
+        data.add((byte) Integer.decode(hexVal).intValue());
+
+        base = base + (txMi * 16384);
+        int step3 = i - base;
+        encodeTwoByteInt(data, step3);
+    }
+
+    private void encodeTwoByteInt(ArrayList<Byte> data, int i) {
         int step1 = i / 2048;
         int txMg = 0x8 + Integer.decode("0x" + step1).intValue();
         int base = 2048 * step1;
